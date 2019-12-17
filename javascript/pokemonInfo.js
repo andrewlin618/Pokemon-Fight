d801f4fa103ab5c39d357c6dc65135e5c8d2778f
Andrew Lin <andrewlin618@gmail.com>
2019-12-16 22:11:03 -0800

    translate into English

--- javascript/pokemonInfo.js
+++ javascript/pokemonInfo.js
@@ -1,28 +1,28 @@
 //Define all pokemon in the game;
 
 //Features for Charizard 
-var pokemon004 = { name: "小火龙", id: "004", hp: 39, level: 5, attack: 60, defense: 50, evolutionLevel: 16 }
-var pokemon005 = { name: "火恐龙", id: "005", hp: 58, level: 16, attack: 80, defense: 65, evolutionLevel: 36 }
-var pokemon006 = { name: "喷火龙", id: "006", hp: 78, level: 36, attack: 109, defense: 85, evolutionLevel: 100 }
+var pokemon004 = { name: "Charmander", id: "004", hp: 39, level: 5, attack: 60, defense: 50, evolutionLevel: 16 }
+var pokemon005 = { name: "Charmeleon", id: "005", hp: 58, level: 16, attack: 80, defense: 65, evolutionLevel: 36 }
+var pokemon006 = { name: "Charizard", id: "006", hp: 78, level: 36, attack: 109, defense: 85, evolutionLevel: 100 }
 
 //Features for Pikachu
-var pokemon025 = { name: "皮卡丘", id: "025", hp: 35, level: 5, attack: 55, defense: 40, evolutionLevel: 100}
-var pokemon026 = { name: "雷丘", id: "026", hp: 60, level: 30, attack: 90, defense: 80, evolutionLevel: 100 }
+var pokemon025 = { name: "Pikachu", id: "025", hp: 35, level: 5, attack: 55, defense: 40, evolutionLevel: 100}
+var pokemon026 = { name: "Raichu", id: "026", hp: 60, level: 30, attack: 90, defense: 80, evolutionLevel: 100 }
 var pokemon2 = pokemon025;
 
 //Features for Dragonite
-var pokemon147 = { name: "迷你龙", id: "147", hp: 41, level: 5, attack: 64, defense: 45, evolutionLevel: 30 }
-var pokemon148 = { name: "哈克龙", id: "148", hp: 61, level: 30, attack: 84, defense: 65, evolutionLevel: 55 }
-var pokemon149 = { name: "快龙", id: "149", hp: 91, level: 55, attack: 134, defense: 95, evolutionLevel: 100 }
+var pokemon147 = { name: "Dratini", id: "147", hp: 41, level: 5, attack: 64, defense: 45, evolutionLevel: 30 }
+var pokemon148 = { name: "Dragonair", id: "148", hp: 61, level: 30, attack: 84, defense: 65, evolutionLevel: 55 }
+var pokemon149 = { name: "Dragonite", id: "149", hp: 91, level: 55, attack: 134, defense: 95, evolutionLevel: 100 }
 var pokemon3 = pokemon147;
 
 //Features for Mewtwo
-var pokemon150 = { name: "超梦", id: "150", hp: 106, level: 70, attack: 154, defense: 90, evolutionLevel: 100 }
+var pokemon150 = { name: "Mewtwo", id: "150", hp: 106, level: 70, attack: 154, defense: 90, evolutionLevel: 100 }
 var pokemon1 = pokemon150;
 
 //Features for Teddiursa
-var pokemon216 = { name: "熊宝宝", id: "216", hp: 60, level: 5, attack: 80, defense: 50, evolutionLevel: 30 }
-var pokemon217 = { name: "圈圈熊", id: "217", hp: 90, level: 5, attack: 130, defense: 75, evolutionLevel: 100 }
+var pokemon216 = { name: "Teddiursa", id: "216", hp: 60, level: 5, attack: 80, defense: 50, evolutionLevel: 30 }
+var pokemon217 = { name: "Ursaring", id: "217", hp: 90, level: 5, attack: 130, defense: 75, evolutionLevel: 100 }
 
 // var pokemonPool = [];
 // pokemonPool.push(pokemon004);
